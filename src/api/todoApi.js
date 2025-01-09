b1aedac86f95edc0684ebc9e5b27641c4f848586
Big1029 <d2ryu01@naver.com>
2025-01-09 15:09:43 +0900

    0109 3차

--- src/api/todoApi.js
+++ src/api/todoApi.js
@@ -10,10 +10,28 @@ export const getOne = async (tno) => {
 }
 
 export const getList = async (pageParam) => {
-    const {page, size} = pageParam
+    const { page, size } = pageParam
+
+    const res = await axios.get(`${prefix}/list`, {
+        params:
+            { page: page, size: size }
+    })
+
+    return res.data
+}
+export const postAdd = async (todoObj) => {
+    const res = await axios.post(`${prefix}/`, todoObj)
+    return res.data
+}
+
+export const deleteOne = async (tno) => {
+    const res = await axios.delete(`${prefix}/${tno}`)
     
-    const res = await axios.get(`${prefix}/list`, {params: 
-        {page:page, size:size }})
-        
+    return res.data
+}
+
+export const putOne = async (todo) => {
+    const res = await axios.put(`${prefix}/${todo.tno}`, todo)
+    
     return res.data
 }
--- src/components/todo/ListComponent.js
+++ src/components/todo/ListComponent.js
@@ -16,14 +16,14 @@ const initState = {
   current: 0
 }
 const ListComponent = () => {
-  const { page, size, moveToList } = useCustomMove()
+  const { page, size, refresh, moveToList, moveToRead } = useCustomMove()
   const [serverData, setServerData] = useState(initState)
   useEffect(() => {
     getList({ page, size }).then(data => {
       console.log(data)
       setServerData(data)
     })
-  }, [page, size])
+  }, [page, size, refresh])
   return (
     <div className="border-2 border-blue-100 mt-10 mr-2 ml-2">
       <div className="flex flex-wrap mx-auto justify-center p-6">
@@ -31,6 +31,7 @@ const ListComponent = () => {
           <div
             key={todo.tno}
             className="w-full min-w-[400px] p-2 m-2 rounded shadow-md"
+            onClick={() => moveToRead(todo.tno)}
           >
             <div className="flex">
               <div className="font-extrabold text-2xl p-2 w-1/12">
@@ -41,13 +42,13 @@ const ListComponent = () => {
               </div>
               <div className="text-1xl m-1 p-2 w-2/12 font-medium">
                 {todo.dueDate}
-              </div>     
               </div>
-              </div>      
-        )}
             </div>
-            <PageComponent serverData={serverData} movePage={moveToList}></PageComponent>
           </div>
-        )
-        }
-        export default ListComponent
+        )}
+      </div>
+      <PageComponent serverData={serverData} movePage={moveToList} />
+    </div>
+  )
+}
+export default ListComponent
