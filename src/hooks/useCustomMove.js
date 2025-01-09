b9d9133aaa8d980ab722b8bb2cae5d0687a87bf8
Big1029 <d2ryu01@naver.com>
2025-01-09 11:56:38 +0900

    0109 2차 수정

--- src/hooks/useCustomMove.js
+++ src/hooks/useCustomMove.js
@@ -1,38 +1,43 @@
+import { useState } from "react"
 import { createSearchParams, useNavigate, useSearchParams } from "react-router-dom"
 
-const getNum = (param, defaultValue) => {
-   if(!param){
-       return defaultValue
-   }
-   return parseInt(param)
-}
+const getNum = (param, defaultValue) => { /* 생략 */ }
 
 const useCustomMove = () => {
-   
-   const navigate = useNavigate()
-   
-   const [queryParams] = useSearchParams()
-
-   const page = getNum(queryParams.get('page'), 1)
-   const size = getNum(queryParams.get('size'),10)
-   
-   const queryDefault = createSearchParams({page, size}).toString() //새로 추가
-
-   const moveToList = (pageParam) => {
-       let queryStr = ""
-
-       if(pageParam){
-           const pageNum = getNum(pageParam.page, 1)
-           const sizeNum = getNum(pageParam.size, 10)
-           
-           queryStr = createSearchParams({page:pageNum, size: sizeNum}).toString()
-       }else {
-           queryStr = queryDefault
-       }
-
-       navigate({pathname: '../list', search: queryStr})
-   }
-
-   return {moveToList, page, size}
+    const navigate = useNavigate()
+    
+    const [refresh, setRefresh] = useState(false) //칠기
+    
+    const [queryParams] = useSearchParams()
+    
+    const page = getNum(queryParams.get('page'), 1)
+    const size = getNum(queryParams.get('size'), 10)
+    
+    const queryDefault = createSearchParams({page, size}).toString()
+    
+    const moveToList = (pageParam) => {
+        let queryStr = ""
+        
+        if(pageParam) {
+            const pageNum = getNum(pageParam.page, 1)
+            const sizeNum = getNum(pageParam.size, 10)
+            
+            queryStr = createSearchParams({page:pageNum, size: sizeNum}).toString()
+        } else {
+            queryStr = queryDefault
+        }
+        
+        setRefresh(!refresh) //추가
+    }
+    
+    const moveToModify = (num) => {
+        console.log(queryDefault)
+        
+        navigate({
+            pathname: `../modify/${num}`,
+            search: queryDefault  //수정하에 기존의 쿼리 스트링 유지를 위해
+        })
+    }
+    return {moveToList, moveToModify, page, size, refresh}
 }
-export default useCustomMove;
+export default useCustomMove
